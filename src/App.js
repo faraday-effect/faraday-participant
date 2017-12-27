579c6825abc19c60f4519f062d7518d78d707911
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-12-27 18:43:53 -0500

    Improve Quiz layout
    - Combine into one component file.
    - Start working on styling.

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     render() {
         return (
             <div className="main-content">
-                <Quiz content={mockQuiz}/>
+                <Quiz quiz={mockQuiz}/>
             </div>
         );
     }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import QuizQuestion from './QuizQuestion';
+import { List, Header } from 'semantic-ui-react'
 
-const Quiz = props => {
+const QuizAnswer = ({answer}) => {
+    return (
+        <List.Item>
+            {answer.text}
+        </List.Item>
+    );
+};
+
+const QuizQuestion = ({question}) => {
+    return (
+        <div className="quiz-question">
+            <p>{question.text}</p>
+            <List bulleted>
+                {question.answers.map(answer => <QuizAnswer key={answer.id} answer={answer}/>)}
+            </List>
+        </div>
+    );
+};
+
+const Quiz = ({quiz}) => {
     return (
         <div className="quiz">
-            <p className="quiz-title">{}
-
-            <div className="quiz-question">
-                {props.content.question}
-            </div>
-            {props.content.answers.map(answer=> <QuizQuestion key={answer.id} answer={answer.text}/>)}
+            <Header as="h1">{quiz.title}</Header>
+            {quiz.questions.map(question => <QuizQuestion key={question.id} question={question}/>)}
         </div>
     );
 };
