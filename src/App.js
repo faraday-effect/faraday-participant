4f75dffa82ce97e111ddc4c38662ede7721d7464
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-12-29 16:33:09 -0500

    Make Quiz work
    - Refactor Quiz class (again)
    - Improve naming of mock data
    - Start using Lodash

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react';
-import Quiz from './components/Quiz';
+import {quizConstants, Quiz} from './components/Quiz';
 import './App.css';
 
 const mockQuiz = {
-    title: 'The Bridge',
+    title: 'The Python Bridge',
     questions: [
         {
-            id: 1,
-            text: 'What is your quest?',
+            key: 'your-name',
+            type: quizConstants.SHORT_ANSWER,
+            required: true,
+            prompt: 'What is your name?'
+        },
+        {
+            key: 'your-quest',
+            type: quizConstants.MULTIPLE_CHOICE,
+            required: true,
+            prompt: 'What is your quest?',
             answers: [
                 {
-                    id: 1,
+                    value: 'grail',
                     text: 'I seek the Holy Grail',
                     correct: true
                 },
                 {
-                    id: 2,
+                    value: 'shrubbery',
                     text: 'I desire a shrubbery',
                     correct: false
                 },
                 {
-                    id: 3,
+                    value: 'groceries',
                     text: 'I seek the grocery store',
                     correct: false
                 }
             ]
-        }
+        },
+        {
+            key: 'your-favorite-color',
+            type: quizConstants.SHORT_ANSWER,
+            required: false,
+            prompt: 'What is your favorite color?'
+        },
     ]
 };
 
 class App extends Component {
     render() {
-        return (
-            <div className="main-content">
-                <Quiz quiz={mockQuiz}/>
-            </div>
-        );
+        return <Quiz quiz={mockQuiz}/>;
     }
 }
 
 export default App;
+
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,47 +1,90 @@
 import React, { Component } from 'react';
 import { Button, Form, Header, Radio } from 'semantic-ui-react';
+import { fromPairs } from 'lodash';
 
-export default class Quiz extends Component {
+export const quizConstants = {
+    SHORT_ANSWER: 'SHORT_ANSWER',
+    MULTIPLE_CHOICE: 'MULTIPLE_CHOICE'
+};
+
+export class Quiz extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            answer: null
-        };
+
+        // this.state = {
+        //     "your-name": '',
+        //     "your-quest": '',
+        //     "your-favorite-color": ''
+        // };
+
+        this.state = fromPairs(props.quiz.questions.map(question => [question.key, '']));
     }
 
-    onAnswerChange = (event, data) => {
-        this.setState({answer: data.value});
+    handleShortAnswerChange = (event, data) => {
+        this.setState({[data.name]: data.value});
     };
 
-    onSubmit = () => {
-        console.log(`Submit: ${JSON.stringify(this.state)}`);
+    handleMultipleChoiceChange = (event, data) => {
+        this.setState({[data.name]: data.value});
+    };
+
+    handleSubmit = () => {
+        console.log(this.state);
+    };
+
+    renderShortAnswerQuestion = question => {
+        return (
+            <div className="quiz-question-short-answer" key={question.key}>
+                <p>{question.prompt}</p>
+                <Form>
+                    <Form.Input
+                        name={question.key}
+                        value={this.state[question.key]}
+                        onChange={this.handleShortAnswerChange}
+                    />
+                </Form>
+            </div>
+        );
+    };
+
+    renderMultipleChoiceQuestion = question => {
+        return (
+            <div className="quiz-question-multiple-choice" key={question.key}>
+                <p>{question.prompt}</p>
+                <Form>
+                    {question.answers.map(answer => (
+                        <Form.Field key={answer.value}>
+                            <Radio
+                                label={answer.text}
+                                name={question.key}
+                                value={answer.value}
+                                checked={this.state[question.key] === answer.value}
+                                onChange={this.handleMultipleChoiceChange}
+                            />
+                        </Form.Field>
+                    ))}
+                </Form>
+            </div>
+        );
+    };
+
+    renderQuestion = (question) => {
+        switch (question.type) {
+            case quizConstants.MULTIPLE_CHOICE:
+                return this.renderMultipleChoiceQuestion(question);
+            case quizConstants.SHORT_ANSWER:
+                return this.renderShortAnswerQuestion(question);
+            default:
+                return <p>INVALID QUESTION TYPE</p>;
+        }
     };
 
     render() {
         return (
             <div className="quiz">
                 <Header as="h1">{this.props.quiz.title}</Header>
-                {this.props.quiz.questions.map(question => (
-                    <div className="quiz-question" key={question.id}>
-                        <p>{question.text}</p>
-                        <Form>
-                            {question.answers.map(answer => (
-                                <Form.Field key={answer.id}>
-                                    <Radio
-                                        label={answer.text}
-                                        value={answer.id}
-                                        checked={this.state.answer === answer.id}
-                                        onChange={this.onAnswerChange}
-                                    />
-                                </Form.Field>
-                            ))}
-                        </Form>
-                    </div>
-                ))}
-                <Button
-                    primary
-                    onClick={this.onSubmit}
-                >Submit</Button>
+                {this.props.quiz.questions.map(question => this.renderQuestion(question))}
+                <Button primary onClick={this.handleSubmit}>Submit</Button>
             </div>
         );
     }
